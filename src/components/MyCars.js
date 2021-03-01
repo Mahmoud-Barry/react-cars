bc7e848ba40feff2323fa2f64e96b32dcf7502ac
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2021-03-01 23:07:37 +0100

    modifier les elements du page en utilisants differents methodes

--- src/components/MyCars.js
+++ src/components/MyCars.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import Car from './Cars';
 
 class MyCars extends Component {
+
+
     render() {
-        console.log(this)
         return(
             <div>
                 <h1>{this.props.title}</h1>
+
+                <p >mon pragraphe</p>
 
                 <Car ></Car>
                 <Car >Mercedes</Car>
